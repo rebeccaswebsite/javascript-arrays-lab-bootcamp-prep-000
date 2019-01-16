1152125c20dcecaadee9b26e295c0c7cccce068f
rebecca_huseyin <rebecca_huseyin@outlook.com>
2019-01-16 13:31:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop("Garfield");
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift("Milo");
+  return kittens; 
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(3)
+}
